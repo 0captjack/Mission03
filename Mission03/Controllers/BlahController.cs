026523dd7282a079e724f3d03b8390a21d9d988e
0captjack <85979485+0captjack@users.noreply.github.com>
2022-01-20 19:16:21 -0700

    Add project files.

---
+++ Mission03/Controllers/BlahController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission03.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //I need to watch the videos again to get the right word but these are like
+        //telling the controller which views are in its arsenal and it can call them if 
+        //it is told to do so. Something like that
+        public IActionResult GradeCalc ()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission03/wwwroot/js/JavaScript.js
@@ -0,0 +1,43 @@
+﻿////document.getElementById("btnSend").addEventListener("click", function () {
+////    alert("This worked");
+////})
+
+//// Assignments 55 %
+//Group Project 5 %
+//    Quizzes 10 %
+//        Exams 20 %
+//            INTEX 10
+
+
+$("#btnSend").click(function () {
+    /*establishing variables for the different grade types and assigning them
+     their corresponding decimal value for grade weight*/
+    var assignment = $("#assval").val() * .55
+    var gproject = $("#groupval").val() * .05
+    var quizzes = $("#quizval").val() * .10
+    var exams = $("#examval").val() * .20
+    var intex = $("#intexval").val() * .10
+    var tot = assignment + gproject + quizzes + exams + intex;
+    
+    var letter = 'F';
+    /*series of if/elseif statements to output the correct letter grade based on input*/
+    if (tot > 89) {
+        letter = 'A'
+    }
+    else if (tot > 79) {
+        letter = 'B'
+    }
+    else if (tot > 69) {
+        letter = 'C'
+    }
+    else if (tot > 59) {
+        letter = 'D'
+    }
+    /*alert box to output the grade*/
+    alert("Percentage: " + tot + "\n" +
+        "Grade: " + letter);
+
+    /*$("#Buster").fadeToggle(); just playin*/
+})
+
+
